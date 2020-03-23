6a9bfb2b8f4a8cf70d5ad0255450f70ade9423eb
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2020-03-23 16:20:13 +0100

    DRY

--- mean_pixels.py
+++ mean_pixels.py
@@ -1,10 +1,18 @@
 # This is my project!
 
+
+def calculate_pixel_brightness(x, y, z):
+    brightness = (x + y + z) / 3
+    return brightness
+
+
 red, green, blue = 100, 0, 0
-pixel_brightness = (red + green + blue) / 3
+
+pixel_brightness = calculate_pixel_brightness(red, green, blue)
+
 
 red2, green2, blue2 = 50, 100, 0
-pixel_brightness2 = (red2 + green2 + blue2) / 3
+pixel_brightness2 = calculate_pixel_brightness(red2, green2, blue2)
 
 readiness = 0
 readiness2 = 200
