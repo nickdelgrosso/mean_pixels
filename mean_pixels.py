e3a71b1ea3f61422972b584e09b9ca45d7607584
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2020-03-23 16:49:14 +0100

    added for loop

--- mean_pixels.py
+++ mean_pixels.py
@@ -5,12 +5,19 @@ def calculate_pixel_brightness(x, y, z):
     brightness = statistics.mean([x, y, z])
     return brightness
 
+data = [1, 2, 3]
+data_times_two = [x * 2 for x in data]  # [1, 4, 9]
+
 
 pixel = {'red': 100, 'green': 0, 'blue': 0}
+pixel2 = {'red': 50, 'green': 100, 'blue': 0}
+pixels = [pixel, pixel2]
+
+pixel_brightnesses = [calculate_pixel_brightness(pixel['red'], pixel['green'], pixel['blue']) for pixel in pixels]
+
 pixel_brightness = calculate_pixel_brightness(pixel['red'], pixel['green'], pixel['blue'])
 
-pixel2 = {'red': 50, 'green': 100, 'blue': 0}
 pixel_brightness2 = calculate_pixel_brightness(pixel2['red'], pixel2['green'], pixel2['blue'])
 
-mean_brightness = statistics.mean([pixel_brightness, pixel_brightness2])
+mean_brightness = statistics.mean(pixel_brightnesses)
 print(mean_brightness)
