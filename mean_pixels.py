cb949397f039a684ac2e60793c4f70a724af32da
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2020-03-23 16:11:21 +0100

    shortened code

--- mean_pixels.py
+++ mean_pixels.py
@@ -1,13 +1,9 @@
 # This is my project!
 
-red = 100
-green = 0
-blue = 0
+red, green, blue = 100, 0, 0
 pixel_brightness = (red + green + blue) / 3
 
-red2 = 50
-green2 = 100
-blue2 = 0
+red2, green2, blue2 = 50, 100, 0
 pixel_brightness2 = (red2 + green2 + blue2) / 3
 
 readiness = 0
